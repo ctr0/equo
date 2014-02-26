cf3f8c18f86bb8e07c7f772ca30d392e6d135b1d
ctr0 <jordi.ctro@gmail.com>
2014-02-26 13:20:58 +0100

    - Function columns test

--- org.equo.test/src/org/equo/test/model/peer/PROFESSIONS.java
+++ org.equo.test/src/org/equo/test/model/peer/PROFESSIONS.java
@@ -2,19 +2,19 @@ package org.equo.test.model.peer;
 
 import javax.annotation.Generated;
 
-import org.equo.Entity;
+import org.equo.Column;
 import org.equo.Table;
-import org.equo.TableColumn;
+import org.equo.Entity;
 import org.equo.test.model.Profession;
 
 @Generated("org.equo.gen.Generator")
 public class PROFESSIONS<E extends Entity<E>> extends Table<E, Profession> {
 	
-	public TableColumn<E, String> CPROF = null;
+	public Column<E, String> CPROF = null;
 	
 	public PROFESSIONS(Table <E, ?> parent, String alias) {
 		super(parent, alias);
-		CPROF = new TableColumn<E, String>(this, "CPROF", Domains.CCODE, 0);
+		CPROF = Column.createColumn(this, "CPROF", Domains.CCODE, 0);
 		setColumns(CPROF);
 	}
 
--- org.equo.test/src/org/equo/test/model/peer/WEAPONSPROF.java
+++ org.equo.test/src/org/equo/test/model/peer/WEAPONSPROF.java
@@ -2,22 +2,21 @@ package org.equo.test.model.peer;
 
 import javax.annotation.Generated;
 
-import org.equo.Entity;
+import org.equo.Column;
 import org.equo.Table;
-import org.equo.TableColumn;
+import org.equo.Entity;
 import org.equo.test.model.WeaponsProf;
 
 @Generated("org.equo.gen.ModelGenerator")
-@SuppressWarnings("unchecked")
 public class WEAPONSPROF<E extends Entity<E>> extends Table<E, WeaponsProf> {
 
-	public final TableColumn<E, String> CPROF;
-	public final TableColumn<E, String> CWEAPON;
+	public final Column<E, String> CPROF;
+	public final Column<E, String> CWEAPON;
 
 	public WEAPONSPROF(Table<E, ?> parent, String alias) {
 		super(parent, alias);
-		CPROF = new TableColumn<E, String>(this, "CPROF", Domains.CCODE, 0);
-		CWEAPON = new TableColumn<E, String>(this, "CWEAPON", Domains.CCODE, 1);
+		CPROF = Column.createColumn(this, "CPROF", Domains.CCODE, 0);
+		CWEAPON = Column.createColumn(this, "CWEAPON", Domains.CCODE, 1);
 		setColumns(CPROF, CWEAPON);
 	}
 
